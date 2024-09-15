19fbd00e5439ba965e6ce4f080dc6b642530e5fd
kanghyki <kanghyki@gmail.com>
2024-09-15 20:45:25 +0900

    참조한 문서 paragraph 하이라이팅 css 스타일 업데이트

--- js/mentionLink.js
+++ js/mentionLink.js
@@ -4,15 +4,20 @@
         return encodeURI(thisName);
     }
 
+    const target = getTarget();
+    const fileName = target.replace(/^.*\/(.*)$/, '$1');
+
     function makeHTML(mlist) {
         if (mlist == null || mlist.length < 1) return '';
-
         let ret = '<h2>이 문서를 참조한 문서</h2><ul class="mention-ul">';
         for (const mention of mlist) {
+            const para = mention.paragraph.replace(new RegExp(String.raw`(\[\[.*${fileName}\]\])`, "g"), '<span class="mention-highlight">$1</span>');
             ret += `<li>
                 <a class="mention-link" href="${mention.url}">
                     <span>${mention.title}</span>
-                    <div>${mention.paragraph}</div>
+                    <div>
+                        ${para}
+                    </div>
                 </a>
             </li>`;
         }
@@ -21,7 +26,7 @@
         return ret;
     }
 
-    const target = getTarget();
+
     let target_res = null;
     try {
         target_res = await fetch(`/data/mention/${target}.json`);
