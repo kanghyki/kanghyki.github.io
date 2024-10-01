17fad6b76e47fc79e3d079e6048cdcfee5bbebbd
kanghyki <kanghyki@gmail.com>
2024-10-02 00:14:50 +0900

    실행 스크립트 수정
    - 실시간 메타 데이터 생성

--- jekyll-run.sh
+++ jekyll-run.sh
@@ -2,4 +2,14 @@
 
 rm -rf data
 ./generateData.js
-jekyll $@
+
+jekyll s --watch &
+JEKYLL_PID=$!
+
+fswatch -o _wiki | xargs -n 1 sh -c 'rm -rf data; ./generateData.js' &
+FSWATCH_PID=$!
+
+trap "kill $FSWATCH_PID $JEKYLL_PID; exit" SIGINT
+
+wait $FSWATCH_PID
+wait $JEKYLL_PID
